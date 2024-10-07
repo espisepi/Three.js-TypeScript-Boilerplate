4d8bb4822e6efe850efc2c159f15ca4289635eee
espisepi <sepinaco@gmail.com>
2024-10-07 20:44:05 +0200

    updated threejs version and orbitcontrols with enableDampling

--- src/client/client.ts
+++ src/client/client.ts
@@ -14,6 +14,9 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true;
+// controls.dampingFactor = 1.5;
+// controls.enablePan = true;
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
